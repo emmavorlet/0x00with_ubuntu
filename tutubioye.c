e5f487269e6e1871681b75e598ad6e9d063843e6
Doctware <yusclever9@gmail.com>
2022-05-16 17:36:23 +0100

    i'm coding

---
+++ tutubioye.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int x = 10;
+	int e = 15;
+	int s = e * 15;
+	int y = x / 2;
+	int c =  500;
+	int v = c * 2;
+
+	printf("the magic number is :%i\nthe value of x %i\nthe biggest number is %i\nthe total amount is %i\n", y ,x, s, v);
+	return 0;
+}
