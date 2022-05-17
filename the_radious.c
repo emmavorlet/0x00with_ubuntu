7bcc42b1c66813d14a7fba6a5b53a74cd7cd20f7
Doctware <yusclever9@gmail.com>
2022-05-17 23:05:01 +0100

    the rdious

---
+++ the_radious.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	int radius;  // the distance from outside to center of the cercle
+	printf("pleas enter  radius:");
+
+	scanf("%i",  &radius); // adress-of operator
+
+	
+	printf("the given radius is %i\n",radius );
+	return 0;
+}
