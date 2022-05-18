c90fe5593e1e12083467b43bc67c53a2f1e13c87
Doctware <yusclever9@gmail.com>
2022-05-19 00:16:59 +0100

    never relent

---
+++ clever.c
@@ -0,0 +1,12 @@
+/* the program that uses the characters, integers, and floating-poits data type */
+
+#include <stdio.h>
+
+main()
+{
+	printf("i'm learning the %c programming language\n, 'c' ");
+	printf("i have just completed chapter %d\n", 2);
+	printd("i am %.lf percent ready to move on", 99.9);
+	printf("to the next chapter!\n");
+}
+
