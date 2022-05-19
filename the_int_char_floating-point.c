06d6a4b706f8383f5732bb27162ba2d47c9b17a5
Olayode Yusuf <101481146+Doctware@users.noreply.github.com>
2022-05-19 09:40:08 +0100

    Update and rename clever.c to the_int_char_floating-point.c

--- the_int_char_floating-point.c
+++ the_int_char_floating-point.c
@@ -2,11 +2,12 @@
 
 #include <stdio.h>
 
-main()
+int main()
 {
-	printf("i'm learning the %c programming language\n, 'c' ");
+	printf("i'm learning the %c programming language\n", 'c');
 	printf("i have just completed chapter %d\n", 2);
-	printd("i am %.lf percent ready to move on", 99.9);
+	printf("i am %.lf percent ready to move on", 99.9);
 	printf("to the next chapter!\n");
+	return 0;
 }
 
